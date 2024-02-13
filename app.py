6811f2553caebcdc97b79d492620f3afd93a8411
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-02-13 18:53:27 +0300

    Add about URL

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+
+about_me = {
+   "name": "Вадим",
+   "surname": "Шиховцов",
+   "email": "vshihovcov@specialist.ru"
+}
 
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+
+@app.route("/about")
+def about():
+    return about_me
 
 
 if __name__ == "__main__":
